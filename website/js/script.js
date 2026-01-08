b2ae4655277790ee62fa2438ecf964e5ef49b70d
Fynn Levine <FKLGTL2010@gmail.com>
2026-01-08 14:17:25 -0800

    feat: Finished customizing layout

--- website/js/script.js
+++ website/js/script.js
@@ -1,21 +1,28 @@
-const url =
-  "https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/fish";
-const options = {
-  method: "GET",
-  headers: {
-    "X-API-Key":
-      "c355fe65e937daae839c9c7858abc50a20f4109f72d7902de404b853c6c7cdfc",
-  },
+const definitionInput = document.querySelector(".definition-input");
+const searchButton = document.querySelector(".search-btn");
+
+const loadRandomWord = async () => {
+  const url =
+    "https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/?random=true";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-API-Key":
+        "c355fe65e937daae839c9c7858abc50a20f4109f72d7902de404b853c6c7cdfc",
+    },
+  };
+
+  fetch(url, options)
+    .then((response) =>
+      response.json().then((result) => {
+        console.log(result.data); // Your API data
+        console.log(`Cost: $${result.meta.cost}`);
+        console.log(`Remaining: $${result.meta.remaining_budget}`);
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-fetch(url, options)
-  .then((response) =>
-    response.json().then((result) => {
-      console.log(result.data); // Your API data
-      console.log(`Cost: $${result.meta.cost}`);
-      console.log(`Remaining: $${result.meta.remaining_budget}`);
-    })
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+loadRandomWord();
