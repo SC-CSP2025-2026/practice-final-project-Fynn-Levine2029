56f4c36e8867783930ff496e5340c8991cbf4239
Fynn Levine <FKLGTL2010@gmail.com>
2026-01-26 11:28:50 -0800

    WIP: Working on DeVaughn Dictionary

--- website/js/script.js
+++ website/js/script.js
@@ -1,9 +1,10 @@
 const definitionInput = document.querySelector(".definition-input");
 const searchButton = document.querySelector(".search-btn");
+const listGroup = document.querySelector(".list-group");
 
 const loadRandomWord = async () => {
   const url =
-    "https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/?random=true";
+    "https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/?random=true"; //?random=true
   const options = {
     method: "GET",
     headers: {
@@ -12,17 +13,32 @@ const loadRandomWord = async () => {
     },
   };
 
-  fetch(url, options)
-    .then((response) =>
-      response.json().then((result) => {
-        console.log(result.data); // Your API data
-        console.log(`Cost: $${result.meta.cost}`);
-        console.log(`Remaining: $${result.meta.remaining_budget}`);
-      })
-    )
-    .catch((error) => {
-      console.log(error);
-    });
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
+  const word = result.data.word;
+  //   console.log(result.data.word);
+  const listItem = `
+          <li class="list-group-item">${word}`;
+  listGroup.insertAdjacentHTML("beforeend", listItem);
+
+  //   words.forEach((word) => {
+  //     const listItem = `
+  //         <li class="list-group-item">${word.definition} - ${word.partOfSpeech} - ${word.synonyms} - ${word.pronunciation}`;
+  //     listGroup.insertAdjacentHTML("beforeend", listItem);
+  //   });
+
+  //   fetch(url, options)
+  //     .then((response) =>
+  //       response.json().then((result) => {
+  //         console.log(result.data); // Your API data
+  //         console.log(`Cost: $${result.meta.cost}`);
+  //         console.log(`Remaining: $${result.meta.remaining_budget}`);
+  //       })
+  //     )
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
 };
 
 loadRandomWord();
