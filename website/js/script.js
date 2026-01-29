87622fcf6d06dd7085663a8c600537a4189b98dc
Fynn Levine <FKLGTL2010@gmail.com>
2026-01-29 10:07:27 -0800

    WIP: working

--- website/js/script.js
+++ website/js/script.js
@@ -1,32 +1,38 @@
-const definitionInput = document.querySelector(".definition-input");
+// const definitionInput = document.querySelector(".definition-input");
+const inputSearchWord = document.querySelector(".input-search-word");
 const searchButton = document.querySelector(".search-btn");
 const listGroup = document.querySelector(".list-group");
 
-const loadRandomWord = async () => {
-  const url =
-    "https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/?random=true"; //?random=true
+const searchWords = async (searchTerm) => {
+  // if (!searchTerm) {
+  //     alert("Please enter a word");
+  //     return;
+  // }
+
+  const url = `https://student-api-proxy.onrender.com/api/wordsapiv1.p.rapidapi.com/words/${searchTerm}/definitions`;
+
   const options = {
     method: "GET",
     headers: {
       "X-API-Key":
-        "c355fe65e937daae839c9c7858abc50a20f4109f72d7902de404b853c6c7cdfc",
+        "f8d1b8d388767999535d45be00b23f41f27285d2d3a650e7d4ca63658e62f75a",
     },
   };
 
   const response = await fetch(url, options);
   const result = await response.json();
   console.log(result);
-  const word = result.data.word;
-  //   console.log(result.data.word);
-  const listItem = `
-          <li class="list-group-item">${word}`;
-  listGroup.insertAdjacentHTML("beforeend", listItem);
+  const words = result.data;
 
-  //   words.forEach((word) => {
-  //     const listItem = `
-  //         <li class="list-group-item">${word.definition} - ${word.partOfSpeech} - ${word.synonyms} - ${word.pronunciation}`;
-  //     listGroup.insertAdjacentHTML("beforeend", listItem);
-  //   });
+  //   const response = await fetch(url, options);
+  //   const result = await response.json();
+  //   console.log(result);
+  //   const word = result.data.word;
+
+  //   console.log(result.data.word);
+  //   const listItem = `
+  //               <li class="list-group-item">${word}`;
+  //   listGroup.insertAdjacentHTML("beforeend", listItem);
 
   //   fetch(url, options)
   //     .then((response) =>
@@ -39,6 +45,12 @@ const loadRandomWord = async () => {
   //     .catch((error) => {
   //       console.log(error);
   //     });
+
+  searchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const searchTerm = inputSearchWord.value.trim();
+    searchWords(searchTerm);
+  });
 };
 
-loadRandomWord();
+searchWords();
