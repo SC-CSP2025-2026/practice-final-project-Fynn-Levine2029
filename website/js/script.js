b26d565b464c8ec817675d796deedb6473ceed8f
Fynn Levine <FKLGTL2010@gmail.com>
2026-02-02 13:59:41 -0800

    feat: Can now search words

--- website/js/script.js
+++ website/js/script.js
@@ -2,6 +2,7 @@
 const inputSearchWord = document.querySelector(".input-search-word");
 const searchButton = document.querySelector(".btn-search-word");
 const listGroup = document.querySelector(".list-group");
+const wordCardDiv = document.querySelector(".word-card");
 
 const searchWords = async (searchTerm) => {
   // if (!searchTerm) {
@@ -18,11 +19,35 @@ const searchWords = async (searchTerm) => {
         "f8d1b8d388767999535d45be00b23f41f27285d2d3a650e7d4ca63658e62f75a",
     },
   };
+};
 
+const getWordAndGiveDefinition = async (url, options) => {
   const response = await fetch(url, options);
   const result = await response.json();
   console.log(result);
-  const words = result.data;
+  let firstResult = result;
+  if (Array.isArray(result)) {
+    firstResult = result[0];
+  }
+  const words = result.data.definitions;
+  getDefinition(words);
+};
+
+const getDefinition = async (searchTerm) => {
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => {
+      const definition = result.searchWords;
+      wordCardDiv.innerHTML = "";
+      definition.forEach((wordResult) => {
+        let card = `
+            <li class="card">
+                <h4>Word: ${wordResult.words}</h4>
+            `;
+        card = card.concat(`</li>`);
+        wordCardDiv.insertAdjacentHTML("beforeend"), card;
+      });
+    });
 };
 
 searchButton.addEventListener("click", (event) => {
@@ -30,3 +55,5 @@ searchButton.addEventListener("click", (event) => {
   const searchTerm = inputSearchWord.value.trim();
   searchWords(searchTerm);
 });
+
+getWordAndGiveDefinition(url, options);
